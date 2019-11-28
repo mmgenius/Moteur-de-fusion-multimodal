e8fec4624b3fe8e3778eb614b090e4dca878946a
Your Name <you@example.com>
2019-11-28 09:52:59 +0100

    added Object class for global use

---
+++ src/Object.java
@@ -0,0 +1,37 @@
+
+public class Object {
+	public Object(int x, int y, String colour, String type) {
+		this.posX = x;
+		this.posY = y;
+		this.colour = colour;
+		this.type = type;
+	}
+	public int getPosX() {
+		return posX;
+	}
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+	public int getPosY() {
+		return posY;
+	}
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+	public String getColour() {
+		return colour;
+	}
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	int posX;
+	int posY;			
+	String colour;		
+	String type; 		//ellipse or rectangle
+}
